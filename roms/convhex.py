25e74fd2cb500d7fccb2699446e2ffc425097e70
rej <rzioma@acm.org>
2024-10-01 17:29:07 +0200

    ROMS and tools

---
+++ roms/convhex.py
@@ -0,0 +1,31 @@
+import binascii
+import sys
+
+# Ensure there's at least one argument passed to avoid IndexError
+if len(sys.argv) < 2:
+    print("Usage: script.py <filename>")
+    sys.exit(1)
+
+i = 0
+filename = sys.argv[1] + '.bin'
+with open(filename, 'rb') as f:
+    content = f.read()
+    for x in content:
+        # Convert the integer to a bytes object of length 1
+        # before passing it to hexlify
+        print(binascii.hexlify(bytes([x])).decode('utf-8'))
+        i += 1
+        if i == 4096:
+            break
+
+#import binascii
+#import sys
+#i = 0
+#filename = sys.argv[1] + '.bin'
+#with open(filename, 'rb') as f:
+#    content = f.read()
+#    for x in content:
+#        print(binascii.hexlify(x))
+#        i += 1
+#        if i == 4096:
+#            break
